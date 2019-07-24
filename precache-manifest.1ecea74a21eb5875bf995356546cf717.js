db6b7f254dd53b941a20613376ecd00b4f74ea26
Andrii <gaarasandy@gmail.com>
2019-07-25 00:33:00 +0300

    Updates

---
+++ precache-manifest.1ecea74a21eb5875bf995356546cf717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26572c036e7394036d95a6ec1565ea03",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "403a4b5927f57e8cd31a",
+    "url": "/youtube-app/static/css/main.171ad7f7.chunk.css"
+  },
+  {
+    "revision": "586ae90a84e9ac392a4d",
+    "url": "/youtube-app/static/js/2.bb1aedac.chunk.js"
+  },
+  {
+    "revision": "403a4b5927f57e8cd31a",
+    "url": "/youtube-app/static/js/main.70061463.chunk.js"
+  },
+  {
+    "revision": "bf759a3baa28ef32c904",
+    "url": "/youtube-app/static/js/runtime~main.b61650af.js"
+  }
+]);
